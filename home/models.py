2d327b85f2cbf2f3e7b5ba94107484a03133a7b1
Will Jones <thefreethinker127@gmail.com>
2015-10-20 22:28:13 -0700

    Made homepage content rich text

--- home/models.py
+++ home/models.py
@@ -1,21 +1,16 @@
 from django.db import models
+from mezzanine.core.models import RichText
 
 # Create your models here.
 
-class FeaturedBox(models.Model):
-    message = models.TextField(blank = True)
-
+class FeaturedBox(RichText):
     class Meta:
         verbose_name_plural = "Featured Box"
 
-class LeftPanel(models.Model):
-    message = models.TextField(blank = True)
-
+class LeftPanel(RichText):
     class Meta:
         verbose_name_plural = "Left Panel"
 
-class CenterPanel(models.Model):
-    message = models.TextField(blank = True)
-
+class CenterPanel(RichText):
     class Meta:
         verbose_name_plural = "Center Panel"
